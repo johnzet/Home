6ccca1955fe8071c72c6d9101d381547950393f9
johnzet <john.zehetner@gmail.com>
2017-08-13 21:11:41 -0600

    minor changes to flash

--- HomeAutomation/ArmModule/FW/Dependencies/Common/include/Flash.h
+++ HomeAutomation/ArmModule/FW/Dependencies/Common/include/Flash.h
@@ -1,28 +1,21 @@
 #include <stm32f4xx.h>
-#include <stm32f4xx_conf.h>
+#include <__cross_studio_io.h>
 #include <string.h>
-
+#include <MessageList.h>
+ 
 #define MEMORY_WRITTEN_MARKER ((uint32_t)(0x12345678))
-
-
-uint32_t flashSector; //FLASH_SECTOR  FLASH_Sector_23
-uint32_t baseFlashAddress; // BASE_ADDRESS_SECTOR_23 0x081E0000
-
+ 
 class Flash {
-
+ 
+    private:
+        uint32_t flashSector;
+        uint32_t baseFlashAddress;
+        uint32_t flashBankSize;
+ 
     public:
-        Flash(uint32_t sector, uint32_t baseAddress, uint32_t sectorLength);
-        ~Flash();
-        void init();
-        void write(uint32_t addressOffset, byte[] data, uint32_t length);
-        uint32_t getBaseAddress();
-        uint8_t getErasedValue();
-
-
-     private:
-        void initPeripheral();
-        void eraseSector();
-        void initSector();
+        void init(MessageList* messageList);
+        uint32_t getFlashMemoryBankStart();
+        uint32_t getFlashMemoryBankSize();
+        void erase();
 };
-
  
