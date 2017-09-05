d058d0f329932e0df1ce1e4cf413f6d924dc10cb
johnzet <john.zehetner@gmail.com>
2017-09-04 20:49:31 -0600

    Set absolute path to hex file

--- HomeAutomation/ArmModule/FirmwarePusher/FirmwarePusher.sh
+++ HomeAutomation/ArmModule/FirmwarePusher/FirmwarePusher.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-java -classpath ./out/production/FirmwarePusher org.zehetner.FirmwarePusher $1 ../FW/WaterGate/WaterGate\ THUMB\ Debug/WaterGate.hex
+java -classpath ./out/production/FirmwarePusher org.zehetner.FirmwarePusher $1 -push ~/projects/Home/HomeAutomation/ArmModule/FW/WaterGate/WaterGate\ THUMB\ Debug/WaterGate.hex
+
