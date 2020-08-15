cfc2eee41d0398c902c227739e72c4965ef1c907
johnzet <john.zehetner@gmail.com>
2020-08-15 17:33:22 -0600

    Added BME280 sensor

--- HomeAutomation/IoT/Weather/src/Weather.ino
+++ HomeAutomation/IoT/Weather/src/Weather.ino
@@ -21,6 +21,7 @@
 #include "Particle.h"
 #include "MLX90393.h"
 #include <math.h>
+#include "SparkFunBME280.h"
 
 // This example does not require the cloud so you can run it in manual mode or
 // normal cloud-connected mode
@@ -42,6 +43,7 @@ BleCharacteristic txCharacteristic("tx", BleCharacteristicProperty::NOTIFY, txUu
 BleCharacteristic rxCharacteristic("rx", BleCharacteristicProperty::WRITE_WO_RSP, rxUuid, serviceUuid, onDataReceived, NULL);
 
 MLX90393 mlx;
+BME280 bme280;
 char buffer[100];
 
 void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context) {
@@ -83,17 +85,65 @@ void setup() {
     mlx.writeRegister(MLX90393::OSR_REG, 0);
     mlx.writeRegister(MLX90393::X_OFFSET_REG, 0);
     mlx.writeRegister(MLX90393::Y_OFFSET_REG, 0);
+
+    bme280.settings.commInterface = I2C_MODE;
+    bme280.settings.I2CAddress = 0x77;
+    //tStandby can be:
+    //  0, 0.5ms
+    //  1, 62.5ms
+    //  2, 125ms
+    //  3, 250ms
+    //  4, 500ms
+    //  5, 1000ms
+    //  6, 10ms
+    //  7, 20ms
+    bme280.settings.tStandby = 0;
+
+    //filter can be off or number of FIR coefficients to use:
+    //  0, filter off
+    //  1, coefficients = 2
+    //  2, coefficients = 4
+    //  3, coefficients = 8
+    //  4, coefficients = 16
+    bme280.settings.filter = 0;
+
+    //tempOverSample can be:
+    //  0, skipped
+    //  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
+    bme280.settings.tempOverSample = 1;
+
+    //pressOverSample can be:
+    //  0, skipped
+    //  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
+    bme280.settings.pressOverSample = 1;
+
+    //humidOverSample can be:
+    //  0, skipped
+    //  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
+    bme280.settings.humidOverSample = 1;
+
+    //runMode can be:
+    //  0, Sleep mode
+    //  1 or 2, Forced mode
+    //  3, Normal mode
+    bme280.settings.runMode = 3;
+    
+    bme280.begin();
+    
 }
 
 void loop() {
     float heading = readCompass();  // contains a delay
-    sprintf(buffer, "Heading %03.1f", heading);
+    sprintf(buffer, "Heading %03.0f     ", heading);
     rewriteLcd(buffer);
 
     if (BLE.connected()) {        
         txCharacteristic.setValue(buffer);
     }
-}
+
+    sprintf(buffer, "%02.0fC %02.0f %03.1fhPa", bme280.readTempC(), bme280.readFloatHumidity(), (float)bme280.readFloatPressure()/100.0f);
+    Serial1.print(buffer);
+    }
 
 void setupLCD() {
      Serial1.begin(9600); //Begin communication with OpenLCD
